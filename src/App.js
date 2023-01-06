b5e9f85c248db8ca8e8f59bd725bf29af29f140d
ejero <rositaemakpo@gmail.com>
2023-01-05 16:34:47 -0800

    Add fetch to poke data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
-const LIMIT = 150;
+const LIMIT = 5;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchPokeApiData = async () => {
+      // fetch pokemon data
+      const pokeData = await fetch(pokeApi)
+      // return data as json
+      const pokeDataToJson = await pokeData.json();
+    
+      // set pokemon data array
+      setPokemonList(pokeDataToJson.results); 
+      
+      // testing to see what is fetched and data ype
+      console.log(pokemonList);
+      console.log(typeof pokemonList);
+    }
+
+    // call the fetch function
+    fetchPokeApiData();
+  }, []);
+
+// Map pokemon card data
+  const getPokeData = pokemonList.map(p => (<>
+    <PokemonCard url={p.url} name={p.name}/>
+  </>))
 
   return (
     <div data-testid="app">
       <Navigation />
-
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <div>{getPokeData}</div>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
+  const [cardData, setCardData] = useState([]);
+  
+  useEffect(() => {
+    const fetchPokeApiData = async () => {
+      // fetch pokemon card data
+      const cardDatas = await fetch(url)
+      // return data as json
+      const cardDataToJson = await cardDatas.json();
+    
+      // set pokemon data array
+      setCardData(cardDataToJson); 
+
+      // Seeing what is in cardData
+      console.log(cardData.abilities[0]);
+      // console.log(cardDataToJson.sprites.front_default)
+    }
+    // call the fetch function
+    fetchPokeApiData();
+  }, []);
+
 
   return (
     <div>
-        pokemon card
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          {cardData.height}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
     </div>
   );
 }
